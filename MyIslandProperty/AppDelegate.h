403b8dc83d93cdbdb636ac617812c0d3ec52bf1b
Adrian Rusin <adrianrusin@Adrians-Mac.local>
2015-12-23 02:12:44 -0800

    Initial Commit

---
+++ MyIslandProperty/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyIslandProperty
+//
+//  Created by Adrian Rusin on 12/23/15.
+//  Copyright (c) 2015 David Roman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyIslandProperty/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyIslandProperty
+//
+//  Created by Adrian Rusin on 12/23/15.
+//  Copyright (c) 2015 David Roman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
